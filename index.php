4ecdda3ca342fd61288d66f13039214d48243773
HsLmA13 <80120259+HsLmA13@users.noreply.github.com>
2021-06-29 19:42:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discord Bot Web Site Tasarımı</title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/style.css?v=<?=time()?>">
+</head>
+<body>
+
+    <header class="col-lg-12">
+        <div class="row">
+            <div class="col-lg-8">
+                <img src="img/rounded.svg" style="width:48px;height:48px;">
+                <a href="#" class="menu">Add to Discord</a>
+                <a href="#" class="menu">Commands</a>
+                <a href="#" class="menu">Premium</a>
+                <a href="#" class="menu">Support</a>
+            </div>
+            <div class="col-lg-4 text-right">
+                <a href="#" class="login">Login</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="background">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h1 class="title">The best music bot for Discord.</h1>
+                    <p class="paragraph">Groovy is the easiest way to play music in your server.</p>
+                    <br><br>
+                    <a href="#" class="menu2">Add to Discord</a>
+                </div>
+                <div class="col-lg-6 text-center">
+                    <img src="img/rounded.svg" style="width:300px;height:300px;">
+                </div>
+            </div>
+        </div>
+        <div class="wave" data-v-44df5d60="">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 250" data-v-44df5d60=""><path fill="#78a4fa" d="M1920.63,26.94l-.13,305.7H.5V41.24c108.29,13,349.38,59.16,557.91,59.14,266.94,0,386.89-70.07,804.78-93.51C1688.37-11.37,1882.84,14.06,1920.63,26.94Z" data-v-44df5d60=""></path></svg>
+        </div>
+    </div>
+
+    <div class="purple text-center">
+        <div class="container">
+            <div class="row text-white">
+                <div class="col-lg-3">
+                    <i class="fas fa-bolt"></i><br>
+                    <strong>Stable</strong><br>
+                    Groovy will always be there to play music for you.
+                </div>
+                <div class="col-lg-3">
+                    <i class="fas fa-user-check"></i><br>
+                    <strong>Easy to Use</strong><br>
+                    No setup required. It only takes one command to get music playing.
+                </div>
+                <div class="col-lg-3">
+                    <i class="fas fa-server"></i><br>
+                    <strong>Lag Free</strong><br>
+                    We ensure that there is always enough capacity for every server to get the highest quality possible.
+                </div>
+                <div class="col-lg-3">
+                    <i class="fas fa-list"></i><br>
+                    <strong>Feature-rich</strong><br>
+                    Groovy supports more sites than any other music bot and has a wide range of commands.
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="gray text-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <h2>Want more than that?</h2>
+                    <p>Consider subscribing to one of our Premium plans in order to be able to use volume control, audio effects and more!</p>
+                    <br>
+                    <a href="#" class="menu2">Get Premium</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="darkgray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <strong>The best music bot for Discord.</strong>
+                    <p>Groovy is the easiest way to play music in your server.</p>
+                    <a href="" class="social"><i class="fab fa-github"></i></a>
+                    <a href="" class="social"><i class="fab fa-twitter"></i></a>
+                    <a href="" class="social"><i class="fab fa-linkedin"></i></a>
+                </div>
+                <div class="col-lg-3">
+                    <strong>Product</strong><br>
+                    <a href="" class="menu3">Add to Discord</a><br>
+                    <a href="" class="menu3">Premium</a><br>
+                </div>
+                <div class="col-lg-3">
+                    <strong>Resources</strong><br>
+                    <a href="" class="menu3">Support</a><br>
+                    <a href="" class="menu3">Contact</a><br>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#" class="menu2">Add to Discord</a>
+                </div>
+                <div class="col-lg-12 mt-4">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            &copy; 2021 Groovy Bot
+                        </div>
+                        <div class="col-lg-6 text-right">
+                            Bir şey
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
